6a12b357cef0a3ebe7aafe2a1a4f4d1e8fd914d8
djsneeky <djfurtweng@gmail.com>
2024-02-01 23:39:44 -0600

    add segmentation functions

--- src/connected.c
+++ src/connected.c
@@ -15,6 +15,8 @@ void ConnectedNeighbors(pixel_t s, double T, unsigned char **img, int width,
 void ConnectedSet(pixel_t s, double T, unsigned char **img, int width,
                   int height, int ClassLabel, unsigned int **seg,
                   int *NumConPixels);
+int GetAllConnectedSets(struct TIFF_img *input_img, double threshold,
+                        int min_connected_pixels);
 
 /**
  * @brief Finds the connected neighbors of a pixel
@@ -60,7 +62,7 @@ void ConnectedNeighbors(pixel_t s, double T, unsigned char **img, int width,
 }
 
 /**
- * @brief
+ * @brief Sets a connected pixel group to a label in the image
  *
  * @param s
  * @param T
@@ -78,11 +80,12 @@ void ConnectedSet(pixel_t s, double T, unsigned char **img, int width,
   pixel_t B[width * height];
   int B_idx = 0;
   B[B_idx] = s;
-  do {
+  int connected_pixels = 0;
+  while (B_idx >= 0) {
     // pop a pixel, set it in the output image, and increment connected count
     pixel_t s = B[B_idx--];
     seg[s.row][s.col] = ClassLabel;
-    *NumConPixels += 1;
+    (*NumConPixels)++;
     // get connected neighbors for the popped pixel
     pixel_t neighbors[4];
     int num_neighbors = 0;
@@ -91,14 +94,147 @@ void ConnectedSet(pixel_t s, double T, unsigned char **img, int width,
     // add neighbors to the queue if not already a part of seg
     for (int i = 0; i < num_neighbors; i++) {
       // printf("neighbor: %d, %d\t", neighbors[i].col, neighbors[i].row);
-      if (seg[neighbors[i].row][neighbors[i].col] != ClassLabel) {
-        // printf("adding neighbor: %d, %d\t", neighbors[i].col,
+      if (seg[neighbors[i].row][neighbors[i].col] == 0) {
+        // printf("adding neighbor: %d, %d\n", neighbors[i].col,
         // neighbors[i].row);
         B[++B_idx] = neighbors[i];
       }
     }
-    // printf("\n");
-  } while (B_idx > 0);
+    // printf("B_idx: %d\n", B_idx);
+  }
+}
+
+int AreaFill(struct TIFF_img *input_img, double threshold, pixel_t s) {
+  FILE *fp;
+  // create output seg image
+  struct TIFF_img seg_img;
+  get_TIFF(&seg_img, input_img->height, input_img->width, 'g');
+
+  // set the output image to 0
+  for (int i = 0; i < seg_img.height; i++) {
+    for (int j = 0; j < seg_img.width; j++) {
+      seg_img.mono[i][j] = 0;
+    }
+  }
+
+  int connected_pixels = 0;
+  ConnectedSet(s, 2, input_img->mono, input_img->width, input_img->height, 255,
+               seg_img.mono, &connected_pixels);
+
+  // open seg image file
+  // Convert double to string
+  char num_str[20];
+  snprintf(num_str, sizeof(num_str), "%.2f", threshold);  // Example format %.2f
+
+  // Construct file name with the double value
+  char output_file[50];
+  strcpy(output_file, "../img/fill_");
+  strcat(output_file, num_str);
+  strcat(output_file, ".tif");
+  if ((fp = fopen(output_file, "wb")) == NULL) {
+    fprintf(stderr, "Error: failed to open output file\n");
+    return EXIT_FAILURE;
+  }
+
+  // write seg image
+  if (write_TIFF(fp, &seg_img)) {
+    fprintf(stderr, "Error: failed to write TIFF file\n");
+    return EXIT_FAILURE;
+  }
+
+  // close seg image file
+  fclose(fp);
+
+  free_TIFF(&(seg_img));
+
+  return EXIT_SUCCESS;
+}
+
+int GetAllConnectedSets(struct TIFF_img *input_img, double threshold,
+                        int min_connected_pixels) {
+  FILE *fp;
+  // create output seg image
+  struct TIFF_img seg_img;
+  get_TIFF(&seg_img, input_img->height, input_img->width, 'g');
+
+  // set the output image to 0
+  for (int i = 0; i < seg_img.height; i++) {
+    for (int j = 0; j < seg_img.width; j++) {
+      seg_img.mono[i][j] = 0;
+    }
+  }
+
+  // Initalize number of regions
+  int total_regions = 0;
+
+  // Iterate through each pixel in raster order
+  for (int y = 0; y < input_img->height; y++) {
+    for (int x = 0; x < input_img->width; x++) {
+      // Check if the pixel already belongs to a connected set
+      if (seg_img.mono[y][x] == 0) {
+        int connected_pixels = 0;
+        struct pixel s = {y, x};
+        // sets a connected set to a static label
+        ConnectedSet(s, threshold, input_img->mono, input_img->width,
+                     input_img->height, 255, seg_img.mono, &connected_pixels);
+        total_regions++;
+        // If the connected set has more than min_set_size pixels, assign a
+        // sequential label starting from 1
+        if (connected_pixels > min_connected_pixels) {
+          printf("connected_pixels meets min: %d\n", connected_pixels);
+          static unsigned char label = 1;
+          // Label the connected set sequentially
+          for (int i = 0; i < seg_img.height; i++) {
+            for (int j = 0; j < seg_img.width; j++) {
+              if (seg_img.mono[i][j] == 255) {
+                seg_img.mono[i][j] = label;
+              }
+            }
+          }
+          printf("label: %d\n", label);
+          label++;
+        } else {
+          // Otherwise, label the connected set as 0
+          for (int i = 0; i < seg_img.height; i++) {
+            for (int j = 0; j < seg_img.width; j++) {
+              if (seg_img.mono[i][j] == 1) {
+                seg_img.mono[i][j] = 0;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // open seg image file
+  // Convert double to string
+  char num_str[20];
+  snprintf(num_str, sizeof(num_str), "%.2f", threshold);  // Example format %.2f
+
+  // Construct file name with the double value
+  char output_file[50];
+  strcpy(output_file, "../img/segmentation_");
+  strcat(output_file, num_str);
+  strcat(output_file, ".tif");
+  if ((fp = fopen(output_file, "wb")) == NULL) {
+    fprintf(stderr, "Error: failed to open output file\n");
+    return EXIT_FAILURE;
+  }
+
+  // write seg image
+  if (write_TIFF(fp, &seg_img)) {
+    fprintf(stderr, "Error: failed to write TIFF file\n");
+    return EXIT_FAILURE;
+  }
+  printf("function GetAllConnectedSets\n");
+
+  // close seg image file
+  fclose(fp);
+
+  free_TIFF(&(seg_img));
+
+  return EXIT_SUCCESS;
 }
 
 int main(int argc, char **argv) {
@@ -133,38 +269,22 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  // create output seg image
-  struct TIFF_img seg_img;
-  get_TIFF(&seg_img, input_img.height, input_img.width, 'g');
-
   pixel_t s = {.col = 67, .row = 45};
-  int connected_pixels = 0;
-  ConnectedSet(s, 2, input_img.mono, input_img.width, input_img.height, 255,
-               seg_img.mono, &connected_pixels);
-
-  // open seg image file
-  // Convert double to string
-  char num_str[20];
-  snprintf(num_str, sizeof(num_str), "%.2f", threshold);  // Example format %.2f
-
-  // Construct file name with the double value
-  char output_file[50];
-  strcpy(output_file, "../img/seg_");
-  strcat(output_file, num_str);
-  strcat(output_file, ".tif");
-  if ((fp = fopen(output_file, "wb")) == NULL) {
-    fprintf(stderr, "Error: failed to open output file\n");
-    return EXIT_FAILURE;
+  int ret = AreaFill(&input_img, threshold, s);
+  if (ret == EXIT_FAILURE) {
+    return ret;
   }
+  printf("finished AreaFill\n");
 
-  // write seg image
-  if (write_TIFF(fp, &seg_img)) {
-    fprintf(stderr, "Error: failed to write TIFF file %s\n", argv[2]);
-    return EXIT_FAILURE;
+  ret = GetAllConnectedSets(&input_img, threshold, 100);
+  if (ret == EXIT_FAILURE) {
+    return ret;
   }
+  printf("finished GetAllConnectedSets\n");
 
-  // close seg image file
-  fclose(fp);
+  free_TIFF(&(input_img));
+
+  printf("done\n");
 
   return EXIT_SUCCESS;
 }
